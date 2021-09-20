010b3f4039f7d21d3a5f3e31fb16e9e1601ddcbd
Emil Tang Kristensen <emiltangkristensen@gmail.com>
2021-09-20 15:14:55 +0200

    Update package

--- app/main.ts
+++ app/main.ts
@@ -25,7 +25,7 @@ function createWindow(): BrowserWindow {
       nodeIntegration: true,
       allowRunningInsecureContent: (serve) ? true : false,
       contextIsolation: false,  // false if you want to run e2e test with Spectron
-      enableRemoteModule: true // true if you want to run e2e test with Spectron or use remote module in renderer context (ie. Angular)
+      //enableRemoteModule: true // true if you want to run e2e test with Spectron or use remote module in renderer context (ie. Angular)
     },
   });
 
