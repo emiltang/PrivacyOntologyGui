b8db97364fba53a68ace7df16b7f64ee1673d8fa
Emil Tang Kristensen <emiltangkristensen@gmail.com>
2021-11-12 23:36:54 +0100

    Add electron service and some pipes

---
+++ src/app/result/filter-context.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { FilterContextPipe } from './filter-context.pipe';
+
+describe('FilterContextPipe', () => {
+  it('create an instance', () => {
+    const pipe = new FilterContextPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
